0cd7df6de4b12043d125c2b983285bde23b987d0
kko0831 <kko0831@hanmail.net>
2020-01-25 13:12:47 +0900

    Modern React 1-5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import Hello from './Hello'; 
 import './App.css';
+import Wrapper from './Wrapper';
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1rem'  
-  }
   return (
-    <>
-      {/* 주석은 화면에 보이지 않습니다 */}
-      /* 중괄호로 감싸지 않으면 화면에 보입니다 */
-      <Hello />
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </>
+    <Wrapper>
+      <Hello name="react" color="red"/>
+      <Hello color="pink"/>
+    </Wrapper>
   );
 }
 
