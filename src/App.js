f0c152d91b73d91a4b99c5dd1b3524a72b969a72
kko0831 <kko0831@hanmail.net>
2020-01-30 00:18:24 +0900

    Modern React 1-13

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React, {useRef} from 'react'; 
+import React, {useRef, useState} from 'react'; 
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
@@ -18,14 +31,33 @@ function App() {
       username: 'liz',
       email: 'liz@example.com'
     }
-  ];
+  ]);
 
   const nextId = useRef(4);
   const onCreate = () => {
+    const user ={
+      id: nextId.current,
+      username,
+      email
+    };
+    setUsers(users.concat(user));
+    
+    setInputs({
+      username: '',
+      email: ''
+    });
     nextId.current += 1;
   };
   return (
-    <UserList users={users}/>
+    <>
+      <CreateUser 
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users}/>
+    </>
   );
 }
 
