06dd3fd5dc9e308bcd32e3f9aee3f807886c9e26
kko0831 <kko0831@hanmail.net>
2020-02-01 23:21:54 +0900

    Modern React 1-15

--- src/App.js
+++ src/App.js
@@ -19,17 +19,20 @@ function App() {
     {
       id: 1,
       username: 'velopert',
-      email: 'public.velopert@gmail.com'
+      email: 'public.velopert@gmail.com',
+      active: true
     },
     {
       id: 2,
       username: 'tester',
-      email: 'tester@example.com'
+      email: 'tester@example.com',
+      active: false
     },
     {
       id: 3,
       username: 'liz',
-      email: 'liz@example.com'
+      email: 'liz@example.com',
+      active: false
     }
   ]);
 
@@ -52,6 +55,13 @@ function App() {
   const onRemove = id => {
     setUsers(users.filter(user => user.id !== id));
   };
+  
+  const onToggle = id => {
+    setUsers(
+      users.map(user => user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+  };
 
   return (
     <>
@@ -61,7 +71,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove} />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
     </>
   );
 }
