067a02aafee2e37ea044f91949ed77790f43801f
kko0831 <kko0831@hanmail.net>
2020-02-05 17:06:08 +0900

    Modern React 1-18

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState, useMemo} from 'react'; 
+import React, {useRef, useState, useMemo, useCallback} from 'react'; 
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
@@ -13,13 +13,16 @@ function App() {
     email: ''
   });
   const { username, email } = inputs;
-  const onChange = e => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    });
-  };
+  const onChange = useCallback(
+    e => {
+      const { name, value } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value
+      });
+    },
+    [inputs]
+  );
   const [users, setUsers] = useState([
     {
       id: 1,
@@ -42,7 +45,7 @@ function App() {
   ]);
 
   const nextId = useRef(4);
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user ={
       id: nextId.current,
       username,
@@ -55,19 +58,22 @@ function App() {
       email: ''
     });
     nextId.current += 1;
-  };
+  }, [users, username, email]);
 
   const onRemove = id => {
     setUsers(users.filter(user => user.id !== id));
   };
   
-  const onToggle = id => {
-    setUsers(
-      users.map(user => 
-        user.id === id ? { ...user, active: !user.active } : user
-      )
-    );
-  };
+  const onToggle = useCallback(
+    id => {
+      setUsers(
+        users.map(user => 
+          user.id === id ? { ...user, active: !user.active } : user
+        )
+      );
+    }, 
+    [users]
+  );
 
   const count = useMemo(() => countActiveUsers(users), [users]);
 
